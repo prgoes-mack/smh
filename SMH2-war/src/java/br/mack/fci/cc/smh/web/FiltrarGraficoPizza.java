d58cd8d94c42180dfb4728c836d8677cce3c345e
prgoes <prgoes@192.168.0.101>
2014-05-03 13:50:38 -0300

    Exemplo de Grafico Web 100%

--- SMH2-war/src/java/br/mack/fci/cc/smh/web/FiltrarGraficoPizza.java
+++ SMH2-war/src/java/br/mack/fci/cc/smh/web/FiltrarGraficoPizza.java
@@ -6,36 +6,20 @@
 
 package br.mack.fci.cc.smh.web;
 
-import br.mack.fci.cc.smh.ejb.PizzaDTO;
-import br.mack.fci.cc.smh.ejb.PizzaMediaHistoricaPorConsulta;
-import com.keypoint.PngEncoder;
-import java.awt.Font;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PiePlot;
-import org.jfree.data.general.DefaultPieDataset;
 
 /**
  *
  * @author prgoes
  */
-@WebServlet(name = "GraficoPizza", urlPatterns = {"/GraficoPizza"})
-public class GraficoPizza extends HttpServlet {
-    
-    @EJB
-    private PizzaMediaHistoricaPorConsulta ejb;
+@WebServlet(name = "FiltrarGraficoPizza", urlPatterns = {"/FiltrarGraficoPizza"})
+public class FiltrarGraficoPizza extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -48,32 +32,26 @@ public class GraficoPizza extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //response.setContentType("text/html;charset=UTF-8");
-        
-        String nomeConsulta = request.getParameter("nome");
-        
-        PizzaDTO dto = ejb.executarPorConsulta(nomeConsulta);        
-        //request.setAttribute("chartDTO", dto);        
-        //request.getRequestDispatcher("/pizzagraph.jsp").forward(request, response);
-        
-         DefaultPieDataset pieDataset = new DefaultPieDataset(); 
-        for(Double key : dto.getResultados().keySet()) {
-                pieDataset.setValue(key, dto.getResultados().get(key));
-            }
-        JFreeChart chart = ChartFactory.createPieChart( 
-            "Sample Pie-Chart created using JFreeChart!", pieDataset, true, 
-            true, true);
-        chart.setTitle(dto.getTitulo()); 
-        chart.setAntiAlias(true); 
-        chart.setTextAntiAlias(false);
-        PiePlot plot = (PiePlot) chart.getPlot(); 
-        plot.setLabelFont(new Font("Tahoma", Font.PLAIN, 11)); 
-        ChartRenderingInfo info = null;
-        BufferedImage chartImage = chart.createBufferedImage(640, 400, info);
-        response.setContentType("image.png");
-        // send the picture
-        PngEncoder encoder = new PngEncoder(chartImage, false, 0, 9);
-        response.getOutputStream().write(encoder.pngEncode());
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FiltrarGraficoPizza</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Selecione a Consulta para Visualizar</h1>");
+            out.println("<form method=\"POST\" action=\"/SMH2-war/GraficoPizza\">");
+            out.println("<label for=\"nome\">Nome:</label>");
+            out.println("<input type=\"text\" name=\"nome\"/>");
+            out.println("<br/>");
+            out.println("<input type=\"submit\"/>");
+            out.println("</form>");
+            out.println("");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
