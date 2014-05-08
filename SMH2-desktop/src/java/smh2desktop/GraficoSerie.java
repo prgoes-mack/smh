034a198843af13cc57860acbe3cc9b5293c419ca
Gisele <Gisele@Gisele-Note>
2014-05-08 18:13:59 -0300

    graficos destop - revisando..

--- SMH2-desktop/src/java/smh2desktop/GraficoSerie.java
+++ SMH2-desktop/src/java/smh2desktop/GraficoSerie.java
@@ -10,12 +10,15 @@ import br.mack.fci.cc.smh.ejb.ISerieConsulta;
 import br.mack.fci.cc.smh.ejb.SerieConsulta;
 import br.mack.fci.cc.smh.ejb.SerieDTO;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.enterprise.context.spi.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 
 /**
  *
@@ -24,45 +27,31 @@ import org.jfree.data.category.DefaultCategoryDataset;
 public class GraficoSerie {
     private ISerieConsulta ejb;
     private JFreeChart grafico;
+    private SerieDTO dto = new SerieDTO();
     
     public DefaultCategoryDataset construiGraficoSerie (String formula)   
     {
        
-        InitialContext context = null;
+      //  InitialContext context = null;
         try {
-            context =  new InitialContext();
+            //context =  new InitialContext();
+            InitialContext context = new InitialContext();
              ejb = (ISerieConsulta)context.lookup ("java:global/SMH2/SMH2-ejb/SerieConsulta");
-            } 
-            catch (NamingException ex) {
-                
+            dto = ejb.executarSerieConsulta(formula);
+            DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+            int x = 1;
+            for (Date key : dto.getResultados().keySet()) {
+
+                dataset.addValue(dto.getResultados().get(key), "Resultados para " + dto.getTitulo(), key);
+                x++;
             }
-        
-       // formula="Ocupacao";
-        SerieDTO dto = ejb.executarSerieConsulta(formula);
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        int x = 1;
-        for (Date key : dto.getResultados().keySet()) {
-
-            dataset.addValue(dto.getResultados().get(key), "Resultados para " + dto.getTitulo(), key);
-            x++;
-        }
-      //  grafico = ChartFactory.createLineChart("Gráfico Série", "Consulta", "Valor", dataset, PlotOrientation.VERTICAL, true, true, false);
-
-       /* BufferedImage buf = null;
-        buf = grafico.createBufferedImage(400, 300);
-
-        ChartRenderingInfo info = null;
-        BufferedImage chartImage = grafico.createBufferedImage(800, 600, info);
-
-        PngEncoder encoder = new PngEncoder(chartImage, false, 0, 9);
-        
-        return grafico;*/
-        
         return dataset;
-            
-        
+        } 
+            catch (NamingException ex) {
+                Logger.getLogger(GraficoSerie.class.getName()).log(Level.SEVERE, null, ex);
+            }
+     
+         return new DefaultCategoryDataset();      
     }
-   
-    
    
     }
